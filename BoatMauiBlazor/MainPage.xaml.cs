0e1c67e998310d8b457908b1dc040f26df800c09
Your Name <you@example.com>
2022-08-09 13:21:08 -0800

    boat-users blazor project initial

---
+++ BoatMauiBlazor/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BoatMauiBlazor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BoatRazorLibrary/Models/BoatUsersLoginModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace BoatRazorLibrary.Models;
+
+public class BoatUsersLoginModel
+{
+    public string? BoatUserName { get; set; }
+    public string? BoatUserPassword { get; set; }
+}
