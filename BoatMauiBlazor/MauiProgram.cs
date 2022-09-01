7456f95b5fb4831ba8ff426dbc756a660f1da1fb
Your Name <you@example.com>
2022-09-01 14:55:51 -0800

    users-login-service-2

--- BoatMauiBlazor/MauiProgram.cs
+++ BoatMauiBlazor/MauiProgram.cs
@@ -20,7 +20,7 @@ public static class MauiProgram
 #endif
 
 		builder.Services.AddSingleton<WeatherForecastService>();
-
+		builder.Services.AddSingleton<IBULogin, BULoginService>();
 		return builder.Build();
 	}
 }
