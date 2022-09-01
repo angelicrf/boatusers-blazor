573c0c4b30842599847a458e4d4da61d7b46dd08
Your Name <you@example.com>
2022-09-01 12:04:07 -0800

    users-map-search-marker-clean-up

---
+++ BoatRazorLibrary/Models/MapFunctions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BoatRazorLibrary.Models;
+
+public class MapFunctions
+{
+    public ElementReference mapElement;
+    public IJSObjectReference? mapModule;
+    public IJSObjectReference? mapInstance;
+    public string UserAddress { get; set; }
+    public MapPlacesModel mapPlaces = new();
+    public async Task MarkerPlace()
+    {
+        if (mapModule is not null && mapInstance is not null)
+        {
+            await mapModule.InvokeVoidAsync("setMapMarker", mapInstance).AsTask();
+        }
+    }
+    public async Task ShowCurrentLocation()
+    {
+        if (mapModule is not null && mapInstance is not null)
+        {
+            mapInstance = await mapModule.InvokeAsync<IJSObjectReference>(
+            "currentLocationAddMap", mapElement);
+        }
+    }
+
+    public async Task FindPlace()
+    {
+        UserAddress = mapPlaces.Name;
+        if (mapModule is not null && mapInstance is not null)
+        {
+
+            await mapModule.InvokeVoidAsync("findPlace", mapInstance, UserAddress).AsTask();
+        }
+    }
+    public async Task AllMarkers()
+    {
+
+        if (mapModule is not null && mapInstance is not null)
+        {
+
+            await mapModule.InvokeVoidAsync("showStoredMarkers", mapInstance).AsTask();
+        }
+    }
+}
--- BoatRazorLibrary/wwwroot/mapInfo.js
+++ BoatRazorLibrary/wwwroot/mapInfo.js
@@ -4,7 +4,6 @@ import 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v2.3.0/ma
 export const callAlert = (thisMsg) => alert(thisMsg)
 
 let thisArray = []
-let markerArray = []
 mapboxgl.accessToken = 'pk.eyJ1IjoiYW5nZWxyZWYiLCJhIjoiY2w0czNxMTA2MGkzcjNqbzB5cjlkM3BkaSJ9.gpg4wdvg4dobgzcw795VQw'
 
 export const initialLocationAddMap = element => {
@@ -48,7 +47,19 @@ export const setMapMarker = (element) => {
     });
      
 }
+export const showStoredMarkers = (map) => {
 
+    if (localStorage.getItem("storedmarkers") != null) {
+
+        let getStMarkers = JSON.parse(localStorage.getItem("storedmarkers"))
+        for (var i = 0; i < getStMarkers.length; i++) {
+            for (var j = 0; j < getStMarkers[i].length; j++) {
+                var searchMarker = new mapboxgl.Marker({ color: "#ff3300", dragable: true, scale: 0.8 }).setLngLat([getStMarkers[i][0], getStMarkers[i][1]]).addTo(map)
+                map.setZoom(-10);
+            }
+        }
+    }
+}
 export const findPlace = (map, thisPlace) => {
 
     let geocoder = new MapboxGeocoder({
@@ -62,12 +73,20 @@ export const findPlace = (map, thisPlace) => {
     map.addControl(geocoder);
     if (thisPlace != null && thisPlace != "") {      
         geocoder.on('result', (e) => {
-            console.log(e.result.center)
-            var searchMarker = new mapboxgl.Marker({ color: "#ff3300", dragable: true, scale: 0.8 }).setLngLat([e.result.center[0], e.result.center[1]]).addTo(map)
+            let searchNewMarker = new mapboxgl.Marker({ color: "#ff3300", dragable: true, scale: 0.8 }).setLngLat([e.result.center[0], e.result.center[1]]).addTo(map)
+            if (localStorage.getItem("storedmarkers") == null) {
+                localStorage.setItem("storedmarkers", JSON.stringify(e.result.center))
+            } else {
+                let getStrMarkers = JSON.parse(localStorage.getItem("storedmarkers"))
 
+                let storeData = []
+                storeData.push(getStrMarkers)
+                storeData.push(e.result.center)
+
+                localStorage.setItem("storedmarkers", JSON.stringify(storeData))
+            }
         })
         geocoder.query(thisPlace)
-
     }
 }
 getCurrentLocation()
