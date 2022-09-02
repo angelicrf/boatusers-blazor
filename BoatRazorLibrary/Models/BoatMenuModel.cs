3988756ccddba9ebd734959dde7ee10cd5837725
Your Name <you@example.com>
2022-09-02 09:48:32 -0800

    users-boats-display

---
+++ BoatRazorLibrary/Models/BoatMenuModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BoatRazorLibrary.Models;
+
+public class BoatMenuModel
+{
+    public string? BoatUrl { get; set; }
+    public string? BoatImgSrc { get; set; }
+    public string? BoatImgAlt { get; set; }
+    public string? BoatTitle { get; set; }
+    public string? BoatDescription { get; set; }
+}
