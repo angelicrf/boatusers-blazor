699e0192b5d171075f51735acf614164b25be1f4
Your Name <you@example.com>
2022-11-11 12:54:51 -0500

    alexa-skills-aws-lambda-function-mdf

--- BoatUsersAWSLambda/Function.cs
+++ BoatUsersAWSLambda/Function.cs
@@ -5,6 +5,7 @@ using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 
+
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -26,21 +27,24 @@ public class Function
     //Access Key ID AKIAUWH7XUSK6A6TCNVX
     //Secret Access Key 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
     //EC2 Access Key APKAUWH7XUSKXQXJBEEK
+    //new access key AKIAUWH7XUSKW46R7HUD
+    //new secret key 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
+    //users name boatusers
+    //link to console https://322658870421.signin.aws.amazon.com/console
     public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
     {
         ILambdaLogger log = context.Logger;
         log.LogLine($"Skill Request Object:" + JsonConvert.SerializeObject(input));
 
-        Session session = input.Session;
-        if (session.Attributes == null)
-            session.Attributes = new Dictionary<string, object>();
+        //Session session = input.Session;
+        //session.Attributes ??= new Dictionary<string, object>();
 
         Type requestType = input.GetRequestType();
         if (requestType == typeof(LaunchRequest))
         {
             string speech = "Welcome! Say your command";
             Reprompt rp = new Reprompt("Say turn on or off or help or cancel to start");
-            return ResponseBuilder.Ask(speech, rp, session);
+            return ResponseBuilder.Ask(speech, rp);
         }
         else if (input.GetRequestType() == typeof(SessionEndedRequest))
         {
@@ -55,7 +59,9 @@ public class Function
 
             switch (intentRequest.Intent.Name)
             {
-                case "AMAZON.StopIntent":
+                case " AMAZON.FallbackIntent":
+                    return ResponseBuilder.Tell("Hi Something went wrong.");
+                //case "AMAZON.StopIntent":
                 case "ShellySwitchOff":
                     return ResponseBuilder.Tell("Device is Tutned off now.");
 
@@ -67,15 +73,15 @@ public class Function
                 case "AMAZON.HelpIntent":
                 case "ShellyAskHelp":
                     {
-                        session.Attributes["num_entered"] = 0;
+                        // session.Attributes["num_entered"] = 0;
                         string userString = intentRequest.Intent.Slots["myNumber"].Value;
-                        userInt = (Int32)(long)session.Attributes["num_guesses"];
+                        //userInt = (Int32)(long)session.Attributes["num_guesses"];
                         Int32.TryParse(userString, out userInt);
-                        correct = (userInt == (Int32)(long)session.Attributes["num_entered"]);
+                        //correct = (userInt == (Int32)(long)session.Attributes["num_entered"]);
                         string next = "what is next";
 
                         Reprompt rp = new Reprompt(next);
-                        return ResponseBuilder.Ask("Do you need help?", rp, session);
+                        return ResponseBuilder.Ask("Do you need help?", rp);
                     }
                 case "AMAZON.CancelIntent":
                 case "ShellySwitchCancel":
@@ -85,22 +91,22 @@ public class Function
                         if (correct)
                         {
                             speech = "Correct! You entered number " + userInt.ToString() + " and matched up number is " + correct.ToString();
-                            session.Attributes["num_entered"] = 0;
+                            //session.Attributes["num_entered"] = 0;
                         }
                         else
                         {
                             speech = "Nope, incorrect number";
-                            session.Attributes["num_entered"] = userInt;
+                            //session.Attributes["num_entered"] = userInt;
                         }
                         Reprompt rp = new Reprompt("enter a number");
-                        return ResponseBuilder.Ask(speech, rp, session);
+                        return ResponseBuilder.Ask(speech, rp);
                     }
                 default:
                     {
                         log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
                         speech = "I didn't understand - try again later!";
                         Reprompt rp = new Reprompt(speech);
-                        return ResponseBuilder.Ask(speech, rp, session);
+                        return ResponseBuilder.Ask(speech, rp);
                     }
             }
         }
