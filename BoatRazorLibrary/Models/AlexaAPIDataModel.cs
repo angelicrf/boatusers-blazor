9cfd0b996fb3f9c6626e9a031ed0ad33dd520890
Your Name <you@example.com>
2023-01-10 13:53:22 -0500

    alexa-powercontroller-requestapi

---
+++ BoatRazorLibrary/Models/AlexaAPIDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BoatRazorLibrary.Models;
+
+public class AlexaAPIDataModel
+{
+    [JsonPropertyName("context")]
+    public AlexaPowerControlResponse DeviceData { get; set; }
+}
+public class AlexaPowerControlResponse
+{
+    [JsonPropertyName("properties")]
+    public object[] PRproperties { get; set; }
+}
---
+++ BoatRazorLibrary/Models/AlexaApiRequests.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json;
+
+namespace BoatRazorLibrary.Models;
+
+public class AlexaApiRequests
+{
+    private readonly HttpClient _httpClient;
+    private string ThisUri = "https://76ewqh4kz26525z22jjuyzooqy0ziulc.lambda-url.us-east-1.on.aws/?alexaMsg=PowerController";
+    private object[] getProperties { get; set; }
+    public AlexaApiRequests()
+    {
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri(ThisUri);
+    }
+    public async Task<object[]> PostAlexaAPIPowerControlEvent(string thisAccessToken)
+    {
+        AlexaAPIDataModel result = new AlexaAPIDataModel();
+
+        try
+        {
+            Guid myuuid = Guid.NewGuid();
+            string myuuidAsString = myuuid.ToString();
+
+            string payload = JsonConvert.SerializeObject(new
+            {
+                powerHeader = new
+                {
+                    namespacedm = "Alexa.PowerController",
+                    name = "TurnOff",
+                    messageId = myuuidAsString
+                },
+
+                powerToken = $"{thisAccessToken}"
+            });
+            //x-www-form-urlencoded
+            using (HttpContent content = new StringContent(payload, Encoding.UTF8, "application/json"))
+            using (HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = _httpClient.BaseAddress,
+                Content = content
+            })
+            using (HttpResponseMessage response = await _httpClient.SendAsync(request))
+            {
+
+                if (response.IsSuccessStatusCode)
+                {
+                    using (var resultContent = response.Content.ReadAsStringAsync())
+
+                        if (resultContent.Result != null)
+                        {
+                            var options = new JsonSerializerOptions
+                            {
+                                PropertyNameCaseInsensitive = true
+
+                            };
+                            var newResult = resultContent.Result;
+                            result = System.Text.Json.JsonSerializer.Deserialize<AlexaAPIDataModel>(resultContent.Result, options);
+                            var getDeviceData = result.DeviceData;
+                            getProperties = getDeviceData.PRproperties;
+                            for (int i = 0; i < getProperties.Length; i++)
+                            {
+                                Console.WriteLine($"all properties : {getProperties[i]}");
+                            }
+
+                        }
+                }
+            }
+        }
+        catch (Exception env)
+        {
+
+            Console.WriteLine(env.Message);
+        }
+        return getProperties;
+    }
+
+}
